ded61247450f6d291110f274d7acd34c25d28b57
Havvanurbilgin <havvanurbilgin620@gmail.com>
2025-03-13 19:09:55 +0300

    odev

---
+++ odev13.03/odev13.03/Program.cs
@@ -0,0 +1,74 @@
+﻿// See https://aka.ms/new-console-template for more information
+using odev13._03;
+using System;
+        
+Yas yas = new Yas();   
+Console.WriteLine(yas.YasKategorisi(13));
+        Console.WriteLine(yas.YasKategorisi(54));
+        Console.WriteLine(yas.YasKategorisi(100));
+
+Console.WriteLine(arabayasi(2));
+Console.WriteLine(arabayasi(11));
+
+
+
+List<araba> arabalar = new List<araba>();
+araba araba1 = new araba { model = "Ford Focus", yakittüketimi = 6.5m, yolmesafesi = 300 };
+araba araba2 = new araba { model = "BMW 320i", yakittüketimi = 8.2m, yolmesafesi = 450 };
+
+
+
+decimal toplambenzintüketimi = 0;
+foreach (var araba in arabalar)
+{
+    Console.WriteLine(araba.model + " - Benzin Tüketimi: " + araba.benzintüketimi() + " L");
+    toplambenzintüketimi += araba.benzintüketimi();
+
+}
+Console.WriteLine("toplam benzin tüketimi : " + toplambenzintüketimi + "l");
+
+static String arabayasi(int yas)
+    {
+        if (yas < 0 || yas > 30)
+            return "Ya hiç üretilmedi ya da trafikten men edilmiştir";
+        else if (yas < 10)
+            return "Arabanız yeni";
+        else if (yas < 20)
+            return "Servise götürmeniz gerekebilir";
+        else
+            return "Arabanız hurdaya çıkabilir";
+
+    }
+       
+
+
+int sayi = 0;
+while ( sayi <5)
+{
+    sayi++;
+}
+Console.WriteLine("sayı:" + sayi);
+
+int sayi2 = 0;
+
+do
+{
+    sayi2++;
+}
+
+while (sayi2< 5);
+Console.WriteLine("sayı:" + sayi2);
+
+
+public class araba
+{
+    public string model;
+    public decimal yakittüketimi;
+    public decimal yolmesafesi;
+
+    public decimal benzintüketimi ()
+    {
+        return (yakittüketimi / 100) * yolmesafesi;
+    }
+
+} 
---
+++ odev13.03/odev13.03/Yas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odev13._03
+{
+   public class Yas 
+    {
+        public String YasKategorisi(int yas)
+        {
+            if (yas < 0 || yas > 99)
+                return "Ya hiç doğmadınız ya da çoktan öldünüz...";
+            else if (yas < 18)
+                return "Küçüksünüz";
+            else if (yas < 35)
+                return "Gençsiniz";
+            else if (yas < 55)
+                return "Yetişkinsiniz";
+            else if (yas < 75)
+                return "Yaşlısınız";
+            else
+                return "Çok yaşlısınız";
+
+        }
+    }
+}
